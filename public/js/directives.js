7ea2a69ab9591a9f6faf04558d12d2e98ac4f025
nmante <nmante88@gmail.com>
2014-03-18 03:11:28 -0700

    Changed app.js to server.js, and updated directives and package.json

--- public/js/directives.js
+++ public/js/directives.js
@@ -10,12 +10,13 @@ app.directive('frequencySlider', [function(){
 		// controller: function($scope, $element, $attrs, $transclude) {},
 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-		template: '<input type="number" ng-model="frequency" min="220" max="1760" step="220" value="220"/>',
+		// template: '<input type="number" ng-model="frequency" min="220" max="1760" step="220" value="220"/>',
 		// templateUrl: '',
 		// replace: true,
 		// transclude: true,
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 		link: function($scope, iElm, iAttrs, controller) {
+			console.log($scope);	
 			
 		}
 	};
